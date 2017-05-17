6a095c655ef78922243e3f18f89010986ac05f2f
Vitali Schneider <tastaturkaputt@googlemail.com>
2017-05-17 19:46:05 +0200

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var Nav = require('Nav');
+
+// Stateless functional component
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <Nav/>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Main;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+// Stateless functional component
+var Nav = (props) => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/tastaturkaputt" target="_blank">Vitali Schneider</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
